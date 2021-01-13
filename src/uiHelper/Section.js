58f3a30a80a1f047734850d0e93e08d25691ddc9
Naeem Ahmed <naeem.ahmed@portonics.com>
2021-01-13 23:57:24 +0600

    uiHelper/Icons added && uiHelper/Section changed

--- src/uiHelper/Section.js
+++ src/uiHelper/Section.js
@@ -1,7 +1,7 @@
 const Section = props => {
-  const { name, fullWidth, children } = props;
+  const { name, fullWidth, children, className="" } = props;
   return (
-    <section id={`${name}__section`} className={`${name}__section`}>
+    <section id={`${name}__section`} className={`${name}__section ${className}`}>
       <div className={fullWidth ? 'container-fluid' : 'container'}>
         <div className={`${name}__content`}>
           {children}
