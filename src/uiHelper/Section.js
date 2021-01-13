90401ce9869387d0148677088e7b1b681f014774
Naeem Ahmed <naeem.ahmed@portonics.com>
2021-01-14 00:42:33 +0600

    Section && _common.scss changes

--- src/uiHelper/Section.js
+++ src/uiHelper/Section.js
@@ -1,7 +1,7 @@
 const Section = props => {
   const { name, fullWidth, children, className="" } = props;
   return (
-    <section id={`${name}__section`} className={`${name}__section ${className}`}>
+    <section id={`${name}__section`} className={`${name} ${className}`}>
       <div className={fullWidth ? 'container-fluid' : 'container'}>
         <div className={`${name}__content`}>
           {children}
