b099f74a1d7e472ea22666abdb206674bf512473
Naeem Ahmed <naeem.ahmed@portonics.com>
2021-06-05 23:07:16 +0600

    Vimeo config setting

--- src/pages/Lesson.js
+++ src/pages/Lesson.js
@@ -6,30 +6,39 @@ import { Backward, Forward, History } from '../icons';
 
 const Lesson = () => {
   const [ videoEnded, setVideoEnded ] = useState(false);
+  const [ replay, setReply ] = useState(0)
 
   const videoEndedHandler = () => {
     setVideoEnded(true)
   }
 
+  const videoReplayHanlder = () => {
+    setVideoEnded(false)
+    setReply(replay+1)
+  }
+
   return (
     <>
       <ReactPlayer 
-        url='https://player.vimeo.com/video/281921423' 
+        url={`https://vimeo.com/41567201?${replay}`} 
         width="100%" 
         height="100vh" 
         style={{overflow: 'hidden'}} 
         onEnded={videoEndedHandler}
         config={{
-          vimeo: {}
+          vimeo: {
+            playerOptions: {
+              autoplay: true
+            }
+          }
         }}
-        controls
       />
 
       {
         videoEnded &&
         <div className="overlay">
           <Container fluid className="position-relative">
-            <div className="question-helper-btn history-btn"><History/></div>
+            <div className="question-helper-btn history-btn cursor-pointer" onClick={videoReplayHanlder}><History/></div>
             
             <div className="question-wrapper" style={{minHeight: '100vh'}}>
               <div className="question-helper-btn backward-btn"><Backward/></div>
